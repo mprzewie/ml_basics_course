07a571182059544afa1ae2a476ac6f949fc3fb1a
Marcin Przewięźlikowski <m.przewie@gmail.com>
2018-10-27 01:32:52 +0200

    metric learning done

--- lab2/dataset_ops.py
+++ lab2/dataset_ops.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import imageio as io
-from typing import Tuple
+from typing import Tuple, Optional, Dict
 import cv2
 
 # for loading datasets from .png files
@@ -11,7 +10,7 @@ def load_dataset(
     space_size: Tuple[int, int]=(100, 100)
 ) -> Tuple[np.ndarray, np.ndarray]:
     
-    ds = io.imread(img_path)
+    ds = cv2.imread(img_path)
     ds = cv2.resize(ds, dsize=space_size, interpolation=cv2.INTER_NEAREST)
     flat_ds = ds.reshape(-1, ds.shape[2])
     colors = np.unique(flat_ds, axis=0)[:-1]
@@ -29,13 +28,37 @@ def load_dataset(
     y = np.array(y)
     return X, y
 
+def sliced_dataset_name(prefix: str, n_slices: int, x0: int, x1: int) -> str:
+    return f"{prefix}.{n_slices}.{x0}.{x1}"
 
-def visualize_dataset(X: np.ndarray, y: np.ndarray):
+def slice_dataset(X: np.ndarray, y: np.ndarray, n_slices: int, space_size: Tuple[int, int]=(100, 100)) -> Dict[Tuple[int, int], Tuple[np.ndarray, np.ndarray]]:
+    d0 = space_size[0] / n_slices
+    d1 = space_size[1] / n_slices
     n_classes = len(np.unique(y))
+    result = {}
+    for x0 in range(n_slices):
+        x0_low = x0 * d0
+        x0_high = (x0 + 1) * d0
+        for x1 in range(n_slices):
+            x1_low = x1 * d1
+            x1_high = (x1 + 1) * d1
+            condition = (
+                (x0_low <= X[:, 0]) * 
+                (X[:, 0] <= x0_high) *
+                (x1_low <= X[:, 1]) * 
+                (X[:, 1] <= x1_high)
+            )
+            result[(x0, x1)] = X[condition], y[condition]
+    return result
+
+def visualize_dataset(X: np.ndarray, y: np.ndarray, n_classes: Optional[int]=None, space_size: Tuple[int, int]=(100, 100)):
+    n_classes = len(np.unique(y)) if n_classes is None else n_classes
     colors = ['k', 'b', 'r', 'c', 'm', 'y', 'k', 'w']
     assert(n_classes <= len(colors))
     
     for c in range(n_classes):
         X_c = X[y==c]
         plt.scatter(X_c[:, 0], X_c[:, 1], color=colors[c], marker=".")
+    plt.xlim(0, space_size[0])
+    plt.ylim(0, space_size[1])
     plt.show()
